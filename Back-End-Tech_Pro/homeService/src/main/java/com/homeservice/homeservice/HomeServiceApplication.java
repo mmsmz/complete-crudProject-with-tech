6b34b0762a97ef252c840ad9af31064e4971ff87
mohamed <marafath07@gmail.com>
2021-03-27 18:16:42 +0530

    implmented the eureka service client for rest of the MS service

--- Back-End-Tech_Pro/homeService/src/main/java/com/homeservice/homeservice/HomeServiceApplication.java
+++ Back-End-Tech_Pro/homeService/src/main/java/com/homeservice/homeservice/HomeServiceApplication.java
@@ -4,7 +4,6 @@ package com.homeservice.homeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
