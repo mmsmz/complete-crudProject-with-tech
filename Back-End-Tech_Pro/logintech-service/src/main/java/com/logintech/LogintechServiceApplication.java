4b6d0e87ca13e5f4de9006dc9ec5f20b7bf8687d
mohamed <marafath07@gmail.com>
2021-06-09 22:00:25 +0530

    initialized the login service

---
+++ Back-End-Tech_Pro/logintech-service/src/main/java/com/logintech/LogintechServiceApplication.java
@@ -0,0 +1,13 @@
+package com.logintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogintechServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogintechServiceApplication.class, args);
+	}
+
+}
---
+++ Back-End-Tech_Pro/logintech-service/src/test/java/com/logintech/LogintechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogintechServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
